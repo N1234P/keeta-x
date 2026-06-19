e22464b47463a2c6686e7ba512dcde371fa4fe7e
N1234P <niravp4582@gmail.com>
2026-06-19 01:53:59 -0400

    renamed the .1

--- credentials.py
+++ credentials.py
@@ -1,11 +1,11 @@
 import os 
 
 
-bearer_token = os.environ['X_BEARER_TOKEN.1']
-api_key = os.environ['X_API_KEY.1']
-api_key_secret = os.environ['X_API_KEY_SECRET.1']
-access_token = os.environ['X_ACCESS_TOKEN.1']
-access_token_secret = os.environ['X_ACCESS_TOKEN_SECRET.1']
+bearer_token = os.environ['X_BEARER_TOKEN1']
+api_key = os.environ['X_API_KEY1']
+api_key_secret = os.environ['X_API_KEY_SECRET1']
+access_token = os.environ['X_ACCESS_TOKEN1']
+access_token_secret = os.environ['X_ACCESS_TOKEN_SECRET1']
 openai_api_key = os.getenv("OPENAI")
 github_api_key = os.getenv("GITHUB") 
 
